eda414629698eed11f9eb2ae4664bc306a4689ce
Steven Kingswolf <stevenkingswolf@gmail.com>
2021-06-04 16:47:50 +0100

    reformat csv reader files

--- csv.cpp
+++ csv.cpp
@@ -1,27 +1,27 @@
 
-
 #include "csv.h"
 
 // endofline: check for and consume \r, \n, \r\n, or EOF
 int Csv::endofline(char c)
 {
 	int eol;
-	
-	eol = (c=='\r' || c=='\n');
-	if (c == '\r') {
+
+	eol = (c == '\r' || c == '\n');
+	if (c == '\r')
+	{
 		fin.get(c);
 		if (!fin.eof() && c != '\n')
-			fin.putback(c);	// read too far
+			fin.putback(c); // read too far
 	}
 	return eol;
 }
 
 // getline: get one line, grow as needed
-int Csv::getline(string& str)
-{	
+int Csv::getline(string &str)
+{
 	char c;
-	
-	for (line = ""; fin.get(c) && !endofline(c); )
+
+	for (line = ""; fin.get(c) && !endofline(c);)
 		line += c;
 	split();
 	str = line;
@@ -33,15 +33,16 @@ int Csv::split()
 {
 	string fld;
 	int i, j;
-	
+
 	nfield = 0;
 	if (line.length() == 0)
 		return 0;
 	i = 0;
-	
-	do {
+
+	do
+	{
 		if (i < line.length() && line[i] == '"')
-			j = advquoted(line, fld, ++i);	// skip quote
+			j = advquoted(line, fld, ++i); // skip quote
 		else
 			j = advplain(line, fld, i);
 		if (nfield >= field.size())
@@ -51,22 +52,24 @@ int Csv::split()
 		nfield++;
 		i = j + 1;
 	} while (j < line.length());
-	
+
 	return nfield;
 }
 
 // advquoted: quoted field; return index of next separator
-int Csv::advquoted(const string& s, string& fld, int i)
+int Csv::advquoted(const string &s, string &fld, int i)
 {
 	int j;
-	
+
 	fld = "";
-	for (j = i; j < s.length(); j++) {
-		if (s[j] == '"' && s[++j] != '"') {
+	for (j = i; j < s.length(); j++)
+	{
+		if (s[j] == '"' && s[++j] != '"')
+		{
 			int k = s.find_first_of(fieldsep, j);
-			if (k > s.length())	// no separator found
+			if (k > s.length()) // no separator found
 				k = s.length();
-			for (k -= j; k-- > 0; )
+			for (k -= j; k-- > 0;)
 				fld += s[j++];
 			break;
 		}
@@ -76,17 +79,16 @@ int Csv::advquoted(const string& s, string& fld, int i)
 }
 
 // advplain: unquoted field; return index of next separator
-int Csv::advplain(const string& s, string& fld, int i)
+int Csv::advplain(const string &s, string &fld, int i)
 {
 	int j;
-	
+
 	j = s.find_first_of(fieldsep, i); // look for separator
-	if (j > s.length())               // none found
+	if (j > s.length())				  // none found
 		j = s.length();
-	fld = string(s, i, j-i);
+	fld = string(s, i, j - i);
 	return j;
 }
-
 
 // getfield: return n-th field
 string Csv::getfield(int n)
@@ -96,4 +98,3 @@ string Csv::getfield(int n)
 	else
 		return field[n];
 }
-

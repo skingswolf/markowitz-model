e07349ca6ded2799108ce2f1a72fc8d45cfb39fe
Steven Kingswolf <stevenkingswolf@gmail.com>
2021-06-05 17:44:50 +0100

    Temporarily comment out read_data file's main function to avoid conflicts

--- read_data.cpp
+++ read_data.cpp
@@ -7,41 +7,41 @@ using namespace std;
  * Main function here purely included to allow for debugging read data
  * functions in this file. 
  **/
-int main(int argc, char *argv[])
-{
-    // string fileName = "asset_returns.csv";
-    // int numberOfAssets = 83;
-    // int numberOfReturns = 700;
+// int main(int argc, char *argv[])
+// {
+//     // string fileName = "asset_returns.csv";
+//     // int numberOfAssets = 83;
+//     // int numberOfReturns = 700;
 
-    // string fileName = "asset_returns_small.csv";
-    // int numberOfAssets = 5;
-    // int numberOfReturns = 10;
+//     // string fileName = "asset_returns_small.csv";
+//     // int numberOfAssets = 5;
+//     // int numberOfReturns = 10;
 
-    string fileName = "dummy_returns.csv";
-    int numberOfAssets = 4;
-    int numberOfReturns = 4;
+//     string fileName = "dummy_returns.csv";
+//     int numberOfAssets = 4;
+//     int numberOfReturns = 5;
 
-    // A matrix to store the return data.
-    vector<vector<double> > returnMatrix = readData(numberOfAssets, numberOfReturns, fileName);
+//     // A matrix to store the return data.
+//     vector<vector<double> > returnMatrix = readData(numberOfAssets, numberOfReturns, fileName);
 
-    // Example on how to calculate the average return.
-    double mean = 0;
+//     // Example on how to calculate the average return.
+//     double mean = 0;
 
-    for (int i = 0; i < numberOfAssets; i++)
-    {
-        mean = 0;
-        for (int j = 0; j < numberOfReturns; j++)
-        {
-            double temp = returnMatrix[i][j];
-            cout << "Asset " << i << ", Return " << j << "=" << temp << "\n";
-            mean = mean + temp / numberOfReturns;
-        }
+//     for (int i = 0; i < numberOfAssets; i++)
+//     {
+//         mean = 0;
+//         for (int j = 0; j < numberOfReturns; j++)
+//         {
+//             double temp = returnMatrix[i][j];
+//             cout << "Asset " << i << ", Return " << j << "=" << temp << "\n";
+//             mean = mean + temp / numberOfReturns;
+//         }
 
-        cout << "------------\n";
-    }
+//         cout << "------------\n";
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
 
 /**
  * Casts a string to a double.
